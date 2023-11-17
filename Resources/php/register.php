ec743504e7340f8aee2568e64bd83c29b0456f85
justiennn <huongtien2002@gmail.com>
2023-11-17 20:48:13 +0700

    Yêu cầu trên 16 tuổi _ Tiên Lê

--- Resources/php/register.php
+++ Resources/php/register.php
@@ -1,126 +1,95 @@
-<?php
-    session_start();
-    require_once './php/config.php';
-
-    $sql = "SELECT * FROM `product`;";
-    $all_product = $con->query($sql);
-    
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./CSS/style.css">
-    <link rel="shortcut icon" href="./Image/title-icon.png" type="image/x-icon">
-    <link rel="stylesheet" href="./Font/zhcn.ttf">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Online eStore</title>
+    <link rel="stylesheet" href="../CSS/loginAndRegister.css">
+    <title>Đăng ký</title>
 </head>
 <body>
-<header>
-        <div id="header__logo">
-            <img src="./Image/logo_shopping.png" alt="logo">
+      <div class="container">
+        <div class="box form-box">
+
+        <?php 
+         include("config.php");
+         if(isset($_POST['submit'])){
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $age = $_POST['age'];
+            $password = $_POST['password'];
+
+         //verifying the unique email
+
+         $verify_query = mysqli_query($con,"SELECT Email FROM customer WHERE Email='$email'");
+
+         if(mysqli_num_rows($verify_query) != 0 ){
+            /*echo "<div class='message'>
+                      <p>Email này đã được sử dụng, vui lòng thử Email khác!</p>
+                  </div> <br>";
+            echo "<a href='javascript:self.history.back()'><button class='btn'>Quay lại</button>";*/
+            alert ("Email này đã được sử dụng, vui lòng thử Email khác!");
+         }
+         else{
+
+            mysqli_query($con,"INSERT INTO customer(Username,Email,Age,Password) VALUES('$username','$email','$age','$password')") or die("Error Occured");
+
+            echo "<div class='message'>
+                      <p style=\"color: green;\">Đăng ký thành công!</p>
+                  </div> <br>";
+            echo "<a href='login.php'><button class='btn'>Đăng nhập ngay</button>";
+         }
+
+        }else{
+         
+        ?>
+
+            <header>Đăng ký</header>
+            <form action="" method="post" onsubmit="return validateForm()">
+                <div class="field input">
+                    <label for="username">Tên người dùng</label>
+                    <input type="text" name="username" id="username" autocomplete="off" required>
+                </div>
+
+                <div class="field input">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" autocomplete="off" required>
+                </div>
+
+                <div class="field input">
+                    <label for="age">Tuổi</label>
+                    <input type="number" name="age" id="age" autocomplete="off" required min="0">
+                </div>
+                <div class="field input">
+                    <label for="password">Mật khẩu</label>
+                    <input type="password" name="password" id="password" autocomplete="off" required>
+                </div>
+
+                <div class="field">
+                    <input type="submit" class="btn" name="submit" value="Đăng ký" required>
+                </div>
+                <div class="links">
+                    Đã là thành viên? <a href="login.php">Đăng nhập</a>
+                </div>
+            </form>
         </div>
-
-        <div class="searchBar">
-            <div class="searchIcon">
-                <i class="fa-solid fa-magnifying-glass"></i>
-            </div>
-            <input type="text" id="input" name="input" placeholder="Nhập sản phẩm cần tìm...">
-        </div>
-
-        <ul id="header__nav">
-            <?php 
-                if(isset($_SESSION['username'])){
-                    echo '<i class="fa-regular fa-user"></i>';
-                    echo '<li style="padding: 0 10px;">' . $_SESSION['username'] . '</li>';
-                    session_destroy();
-                }
-            ?>
-            
-            <li><a href="#">GIỎ HÀNG</a></li>
-            
-            <?php
-                if(isset($_SESSION['valid'])){
-                    echo '<li><a href="index.php">ĐĂNG XUẤT</a></li>';
-                }
-                else {
-                    echo '<li><a href="./php/login.php">ĐĂNG NHẬP</a></li>';
-                }
-            ?>
-
-        </ul>
-    </header>
-
-    <div id="content">
-        <div class="phanloai">
-            <h2>PHÂN LOẠI SẢN PHẨM</h2>
-            <div class="loai">
-                <p id="aonu">&nbsp;&nbsp;&nbsp;Áo nữ</p>
-                <p id="aonam">&nbsp;&nbsp;&nbsp;Áo nam</p> 
-                <p id="quanvaynu">&nbsp;&nbsp;&nbsp;Quần váy nữ</p>
-                <p id="quannam">&nbsp;&nbsp;&nbsp;Quần nam</p> 
-                <p id="phukien">&nbsp;&nbsp;&nbsp;Phụ kiện</p>
-            </div>
-        </div>
-        <div class="products-container">
-            <div class="sort">
-                <p>Sắp xếp</p>
-            </div>
-            <?php
-            while($row = mysqli_fetch_assoc($all_product)){
-            ?>
-            <div class="product">
-                <table id="myTable">
-                    <tr>
-                    <td>
-                        <div class="product--hoverEffect">
-                            <img class="product-img" src="<?php echo $row["Image"]; ?>" alt="test">
-                        </div>
-                    </td>
-                    <td>
-                        <div class="product-description">
-                            <span><?php echo $row["Name"]?></span> 
-                            <h5><?php echo $row["Description"]?></h5>
-                            <div class="star">
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-regular fa-star"></i>
-                            </div>
-                                <h4><?php echo $row["Price"]?> VND</h4>
-                            </div>
-                            <a href="#" class="product-cart-icon"><i class="fa-solid fa-cart-shopping"></i></a>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-            <?php }?>
-
-        </div>
-    </div>
-    <footer class="footer">
-		<div class="container row">
-			<div class="footer-col">
-				<h4>Liên hệ với chúng tôi</h4>
-				<ul>
-					<li><a href="#">Số điện thoại +84-XXX-XXX-XXX</a></li>
-					<li><a href="#">Địa chỉ TPHCM</a></li>
-				</ul>
-			</div>
-			<div class="footer-col">
-				<h4>Theo dõi chúng tôi qua</h4>
-				<div class="social-links">
-					<a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-					<a href="#"><i class="fa-brands fa-x-twitter"></i></a>
-					<a href="#"><i class="fa-brands fa-instagram"></i></a>
-					<a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
-				</div>
-			</div>
-		</div>
-	</footer>
+        <?php } ?>
+      </div>
 </body>
-<script src='./JS/index.js'></script>
+<script>
+function validateForm() {
+    let ageInput = document.getElementById("age");
+    // Thực hiện kiểm tra độ tuổi, ở đây là kiểm tra đủ 16 tuổi hay không
+    if (!isOldEnough(ageInput.value)) {
+        alert("Bạn phải đủ 16 tuổi để đăng ký!")
+        return false; // Ngăn chặn biểu mẫu được gửi đi
+    }
+    // Nếu mọi thứ đều hợp lệ, cho phép gửi biểu mẫu
+    return true;
+}
+function isOldEnough(age) {
+    // Lấy ngày hiện tại
+    return (age >= 16);
+}
+</script>
 </html>
