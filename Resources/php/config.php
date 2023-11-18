6a3ca56873820f48c63a4d6679a9c5f9e97d6fe6
phpphuc <19522036@gmail.com>
2023-11-18 22:08:22 +0700

    chore (style): Sửa process_checkout.php - Phuc

--- Resources/php/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $SERVER = "localhost";
-    $USERNAME = "root";
-    $PASSWORD = "";
-    $DBNAME = "website08";
-    $con = mysqli_connect($SERVER, $USERNAME,$PASSWORD, $DBNAME) or die("Connect failed");
-?>
