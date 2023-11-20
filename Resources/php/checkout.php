527131a5eb709cc5e78668be4f315ccc99a421b2
phpphuc <19522036@gmail.com>
2023-11-21 00:35:44 +0700

    chore: Đổi vị trí input form đặt hàng - Phuc

--- Resources/php/checkout.php
+++ Resources/php/checkout.php
@@ -120,6 +120,12 @@ ob_end_flush();
             </div>
 
             <div class="sm:col-span-3">
+              <label class="block text-sm font-medium text-slate-700 dark:text-slate-50" for="shipping-phone"> Số điện thoại </label>
+              <div class="mt-1">
+                <input required class="p-2 block w-full appearance-none rounded-md border border-slate-300  dark:!border-gray-500 shadow-sm checked:bg-sky-500 checked:text-sky-500 focus:border-sky-500 focus:ring-sky-500 disabled:cursor-not-allowed disabled:bg-slate-100 disabled:opacity-50 dark:border-white/10 dark:bg-gray-800 dark:text-slate-50 dark:checked:bg-sky-500 dark:focus:border-sky-500 dark:focus:ring-sky-500 dark:focus:ring-offset-slate-900 sm:text-sm" type="tel" placeholder="injection"  name="shipping-phone" id="shipping-phone" />
+              </div>
+            </div>
+            <div class="sm:col-span-3">
               <label class="block text-sm font-medium text-slate-700 dark:text-slate-50" for="shipping-address-line-1"> Địa chỉ </label>
               <div class="mt-1">
                 <input required class="p-2 block w-full appearance-none rounded-md border border-slate-300  dark:!border-gray-500 shadow-sm checked:bg-sky-500 checked:text-sky-500 focus:border-sky-500 focus:ring-sky-500 disabled:cursor-not-allowed disabled:bg-slate-100 disabled:opacity-50 dark:border-white/10 dark:bg-gray-800 dark:text-slate-50 dark:checked:bg-sky-500 dark:focus:border-sky-500 dark:focus:ring-sky-500 dark:focus:ring-offset-slate-900 sm:text-sm" type="text" placeholder="injection"  name="shipping-address" id="shipping-address" autocomplete="street-address" />
@@ -155,12 +161,6 @@ ob_end_flush();
 </div> 
 <input type="hidden" name="shipping-ward-text" id="shipping-ward-text" value="" />
 
-            <div class="sm:col-span-3">
-              <label class="block text-sm font-medium text-slate-700 dark:text-slate-50" for="shipping-phone"> Số điện thoại </label>
-              <div class="mt-1">
-                <input required class="p-2 block w-full appearance-none rounded-md border border-slate-300  dark:!border-gray-500 shadow-sm checked:bg-sky-500 checked:text-sky-500 focus:border-sky-500 focus:ring-sky-500 disabled:cursor-not-allowed disabled:bg-slate-100 disabled:opacity-50 dark:border-white/10 dark:bg-gray-800 dark:text-slate-50 dark:checked:bg-sky-500 dark:focus:border-sky-500 dark:focus:ring-sky-500 dark:focus:ring-offset-slate-900 sm:text-sm" type="tel" placeholder="injection"  name="shipping-phone" id="shipping-phone" />
-              </div>
-            </div>
           </div>
         </div>
 
