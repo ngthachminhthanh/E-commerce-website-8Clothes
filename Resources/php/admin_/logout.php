0f86387c869ceb54522cdbe48781c9bd09f9a2fd
Minh Thanh <ngthachminhthanh@gmail.com>
2023-11-29 12:58:06 +0700

    Fix logout admin - MT

--- Resources/php/admin_/logout.php
+++ Resources/php/admin_/logout.php
@@ -2,8 +2,8 @@
 	session_start();
 	if(isset($_SESSION['username_'])){
 		session_destroy();
-		header('location:index.php');
+		header('location: ../index.php');
 	}else{
-		header('location:index.php');
+		header('location: ../index.php');
 	}
 ?>
